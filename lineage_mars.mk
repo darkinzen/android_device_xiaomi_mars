cc81a6d4b798fc4ad94bf83bd041a56fc91b95f2
hseyinblgc <h.bilgic1818@gmail.com>
2026-02-12 19:02:43 +0300

    mars: Define axion flags

--- lineage_mars.mk
+++ lineage_mars.mk
@@ -15,6 +15,16 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2102K1AC
 PRODUCT_NAME := lineage_mars
 
+# Camera information (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 50,48,48
+AXION_CAMERA_FRONT_INFO := 20
+
+# Maintainer name (underscores become spaces in the UI)
+AXION_MAINTAINER := darkinzen
+
+# Processor name (underscores become spaces)
+AXION_PROCESSOR := Snapdragon_888
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="mars_global-user 14 UKQ1.240624.001 V2.0.1.0.UKACNXM release-keys" \
     BuildFingerprint=Xiaomi/mars_global/mars:14/UKQ1.240624.001/V2.0.1.0.UKACNXM:user/release-keys \
